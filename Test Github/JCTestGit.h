fa4622a29bf827dee415428472655a6b10e6ebd3
Jorge Corona <jorgecorona@MacBook-Air.local>
2014-05-09 15:22:55 -0500

    added a test file

---
+++ Test Github/JCTestGit.h
@@ -0,0 +1,13 @@
+//
+//  JCTestGit.h
+//  Test Github
+//
+//  Created by Jorge Corona on 5/9/14.
+//  Copyright (c) 2014 Alpine Point. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JCTestGit : NSObject
+
+@end
---
+++ Test Github/JCTestGit.m
@@ -0,0 +1,13 @@
+//
+//  JCTestGit.m
+//  Test Github
+//
+//  Created by Jorge Corona on 5/9/14.
+//  Copyright (c) 2014 Alpine Point. All rights reserved.
+//
+
+#import "JCTestGit.h"
+
+@implementation JCTestGit
+
+@end
